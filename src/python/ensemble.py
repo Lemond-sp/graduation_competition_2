049c6b04b8bb76f219c9399c4ff26e4f259ca4ed
Leon Kajikawa <0131reon254@gmail.com>
2023-02-14 01:41:32 +0900

    ensemble

--- src/python/ensemble.py
+++ src/python/ensemble.py
@@ -12,7 +12,7 @@ for SVR in SVRS:
       avg = round((int(s1) + int(s2)) / 2 )
       score.append(avg)
 
-  with open('../../pseudo/ensemble/sudachi/ensemble'+str(i)+'.txt','w') as fw:
+  with open('../../pseudo/ensemble/ensemble'+str(i)+'.txt','w') as fw:
     for s in score:
       s = int(s)
       fw.write(str(s) + '\n')
